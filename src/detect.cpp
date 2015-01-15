ff8180dca5a683ad06f8eb58a137768d1b54fb27
Prastaruszek <p0kawalek@gmail.com>
2015-01-15 11:48:29 +0100

    Compilation time enshortering. Small change in Makefile

--- src/detect.cpp
+++ src/detect.cpp
@@ -2,6 +2,8 @@
 
 using namespace cimg_library;
 
+void process(unsigned char* src, unsigned char* dst, int width, int height);
+
 template<typename T>
 void displayMeOnTheScreen(CImg<T> image){
 	CImgDisplay main_disp(image,"Click a point");
@@ -24,8 +26,9 @@ int main() {
 	unsigned char* blue = image.data(0,0,0,2);
 	printf("colors have their own line in mem: %lu %lu %lu\n",
 							red-red, grey-red, blue-red); 
-	char grayscale[image.size()];
+							
 	int size =image.size()/image.spectrum();
+	unsigned char grayscale[size];
 	for(int i=0; i<size; ++i){
 		grayscale[i] = round(0.299*((double)red[i]) 
 		+ 0.587*((double)grey[i]) + 0.114*((double)blue[i]));
@@ -33,5 +36,7 @@ int main() {
 	CImg<unsigned char> outImg(grayscale, image.width(), image.height(),
 									image.depth(), 1);
 	displayMeOnTheScreen(outImg);
+	unsigned char dst[size];
+	process(grayscale, dst, image.width(), image.height());
 	return 0;
 }
