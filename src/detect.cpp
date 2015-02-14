c521865749f7b0d10d388fdacfd996c3f6d18c92
Piotr Kawalek <z1087575@miracle.tcs.uj.edu.pl>
2015-02-14 16:08:36 +0100

    It's possible now to choose image passing argument to program.

--- src/detect.cpp
+++ src/detect.cpp
@@ -12,27 +12,47 @@ void displayMeOnTheScreen(CImg<T> image){
 }
 //make detect - w katalogu matce zbuduje nam glowny program. Mozna jeszcze zobaczyc
 //calkiem ciekawy przyklad - make example - ale tu cos bardziej pod nasze potrzeby
-int main() {
-	CImg<unsigned char> image("images/lady_color.bmp");
+
+void getGrayscaleImage(CImg<unsigned char>& image, unsigned char* grayscale){
+	int size = image.size()/image.spectrum();
+	if(image.spectrum()==1){
+		unsigned char* gray = image.data(0,0,0,0);
+		for(int i=0; i<image.size(); ++i){
+			grayscale[i]= gray[i];
+		}
+	}
+	else if(image.spectrum()==3){
+		unsigned char* red = image.data(0,0,0,0);
+		unsigned char* grey = image.data(0,0,0,1);
+		unsigned char* blue = image.data(0,0,0,2);
+		printf("colors have their own line in mem: %lu %lu %lu\n",
+								red-red, grey-red, blue-red);
+		for(int i=0; i<size; ++i){
+			grayscale[i] = round(0.299*((double)red[i]) 
+			+ 0.587*((double)grey[i]) + 0.114*((double)blue[i]));
+		}		
+	}
+}
+int main(int argc, char* argv[]) {
+	char img[50];
+	if(argc>1){
+		strncpy(img, "images/", 50);
+		strncpy(img+7, argv[1], 43);
+	}
+	else{
+		strncpy(img, "images/lady_color.bmp", 50);
+	}
+	CImg<unsigned char> image(img);
 	printf("width=%d hight=%d depth=%d size=%lu spectrum=%d\n",
 				image.width(), image.height(), image.depth(),
-								image.size(), image.spectrum());;
-	unsigned char* red = image.data(0,0,0,0);
-	unsigned char* grey = image.data(0,0,0,1);
-	unsigned char* blue = image.data(0,0,0,2);
-	printf("colors have their own line in mem: %lu %lu %lu\n",
-							red-red, grey-red, blue-red); 
-							
-	int size =image.size()/image.spectrum();
+								image.size(), image.spectrum());
+	int size = image.size()/image.spectrum();
 	unsigned char grayscale[size];
-	for(int i=0; i<size; ++i){
-		grayscale[i] = round(0.299*((double)red[i]) 
-		+ 0.587*((double)grey[i]) + 0.114*((double)blue[i]));
-	}
-	CImg<unsigned char> outImg(grayscale, image.width(), image.height(),
+	unsigned char dst[size];
+	getGrayscaleImage(image, grayscale);
+	process(grayscale, dst, image.width(), image.height());
+	CImg<unsigned char> outImg(dst, image.width(), image.height(),
 									image.depth(), 1);
 	displayMeOnTheScreen(outImg);
-	unsigned char dst[size];
-	process(grayscale, dst, image.width(), image.height());
 	return 0;
 }
--- src/processImg.cpp
+++ src/processImg.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
 void process(unsigned char* src, unsigned char* dst, int width, int height){
-	std::cout << "DOING NOTHING" << std::endl;
+	std::cout << "COPYING" << std::endl;
+	for(int i=0; i<width*height; ++i){
+		dst[i]=src[i];
+	}
 }
