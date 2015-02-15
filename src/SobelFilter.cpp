b4aca2c64d342b1f6b1a5e060917caef1e469d38
Piotr Kawalek <z1087575@miracle.tcs.uj.edu.pl>
2015-02-15 16:42:46 +0100

    small change in algorithm

--- src/SobelFilter.cpp
+++ src/SobelFilter.cpp
@@ -15,10 +15,10 @@ void SobelFilter::filter(unsigned char* dst){
 	}
 	int magX;
 	int magY;
-	for(int i=1; i<height-1; ++i){
-		for(int j=1; j<width-1; ++j){
-			magX = -img[(i)*width+j-1]+img[i*width+j+1];
-			magY = img[(i-1)*width+j]-img[(i+1)*width+j];
+	for(int i=0; i<height-1; ++i){
+		for(int j=0; j<width-1; ++j){
+			magX = -img[(i)*width+j]+img[i*width+j+1];
+			magY = img[(i)*width+j]-img[(i+1)*width+j];
 			magX = magX>0? magX: -magX;
 			magY = magY>0? magY: -magY;
 			dst[i*width+j] = magX+magY>=T?255:0;
